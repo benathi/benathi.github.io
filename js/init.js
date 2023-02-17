0738b02f6feb39d6e4365cffafe969ebd35b9ffa
asad tufail <asad.tufail199913@gmail.com>
2023-02-17 09:35:47 +0500

    all

--- js/init.js
+++ js/init.js
@@ -254,7 +254,7 @@ function arlo_tm_animate_text() {
   "use strict";
   var animateSpan = jQuery(".arlo_tm_animation_text_word");
   animateSpan.typed({
-    strings: ["Freelancer", "Web Developer", "Photographer"],
+    strings: ["Ben Athiwaratkun", "an AI researcher", "a Student"],
     loop: true,
     startDelay: 1e3,
     backDelay: 2e3,
