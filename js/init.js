9ebe3343424cca4a97fe74f5089889356744cf1d
AsadTufail <42470754+AsadTufail@users.noreply.github.com>
2023-02-23 13:56:37 +0500

    minor fixes

--- js/init.js
+++ js/init.js
@@ -555,8 +555,8 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function () {
         this.classList.toggle('active');
         var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
+        if (panel.style.maxHeight != '0px') {
+            panel.style.maxHeight = '0px';
         } else {
             panel.style.height = 'auto';
             panel.style.maxHeight = panel.scrollHeight + 100 + 'px';
